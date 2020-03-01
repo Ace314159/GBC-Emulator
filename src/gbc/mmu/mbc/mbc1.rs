9492f60a8aef50510d65bdffae0cd9df9b36950b
Akash Munagala <akash.munagala@gmail.com>
2020-03-01 01:59:42 -0800

    Add External RAM for MBC1

--- src/gbc/mmu/mbc/mbc1.rs
+++ src/gbc/mmu/mbc/mbc1.rs
@@ -6,6 +6,7 @@ pub struct MBC1 {
     ram_bank: usize,
     ram_enable: bool,
     is_rom_banking: bool,
+    external_RAM: [u8; 0x2000],
 }
 
 impl MBC1 {
@@ -16,6 +17,7 @@ impl MBC1 {
             ram_bank: 0,
             ram_enable: false,
             is_rom_banking: true,
+            external_RAM: [0; 0x2000],
         }
     }
 }
@@ -25,7 +27,7 @@ impl MemoryBankController for MBC1 {
         match addr & 0xC000 {
             0x0000 => self.rom[addr as usize],
             0x4000 => self.rom[self.rom_bank * 0x4000 + addr as usize],
-            0x8000 => 0, // TODO: Add External RAM Support
+            0x8000 => self.external_RAM[addr as usize - 0xA000],
             _ => panic!("Shouldn't be here!"),
         }
     }
@@ -38,6 +40,7 @@ impl MemoryBankController for MBC1 {
             0x4000 => if self.is_rom_banking { self.rom_bank = self.rom_bank & !0x60 | (value as usize) << 5; }
                       else { self.ram_bank = value as usize & 0x03; },
             0x6000 => self.is_rom_banking = value == 0,
+            0xA000 => self.external_RAM[addr as usize - 0xA000] = value,
             _ => panic!("Shouldn't be here!"),
         }
         assert_eq!(self.is_rom_banking, true); // TODO: Add support for RAM Banking
