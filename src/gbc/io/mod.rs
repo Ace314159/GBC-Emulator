257ceb15d1559377cfcbf05508a252594afefc8b
Akash Munagala <akash.munagala@gmail.com>
2020-03-14 19:29:51 -0700

    Make MemoryHandler a public trait

--- src/gbc/io/mod.rs
+++ src/gbc/io/mod.rs
@@ -10,7 +10,7 @@ use ram::RAM;
 use serial::Serial;
 use timer::Timer;
 
-trait MemoryHandler {
+pub trait MemoryHandler {
     fn read(&self, addr: u16) -> u8;
     fn write(&mut self, addr: u16, value: u8);
 }
