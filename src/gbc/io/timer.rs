c5ef5f3e7fd67c7c818c8ee68dbf94031024e307
Akash Munagala <akash.munagala@gmail.com>
2020-03-16 19:59:28 -0700

    Emulate more obscure timer behavior

--- src/gbc/io/timer.rs
+++ src/gbc/io/timer.rs
@@ -12,6 +12,8 @@ pub struct Timer {
 
     // Other
     prev_counter_bit: bool,
+    overflowed: bool,
+    reloaded: bool,
 }
 
 impl Timer {
@@ -23,21 +25,28 @@ impl Timer {
             enabled: false,
             clock_select: 0,
             prev_counter_bit: false,
+            overflowed: false,
+            reloaded: false,
         }
     }
 
     const CLOCK_SELECT: [usize; 4] = [9, 3, 5, 7];
 
     pub fn emulate(&mut self) -> u8 {
-        let mut interrupt = 0;
+        let interrupt = if self.overflowed {
+            self.counter = self.modulo;
+            self.reloaded = true;
+            IO::TIMER_INT
+        } else { self.reloaded = false; 0 };
+        self.overflowed = false;
         self.divider_counter = self.divider_counter.wrapping_add(4);
 
         let bit = Timer::CLOCK_SELECT[self.clock_select as usize];
         let counter_bit = self.enabled && self.divider_counter & (1 << bit) != 0;
         if self.prev_counter_bit && !counter_bit {
             self.counter = if self.counter == 0xFF {
-                interrupt = IO::TIMER_INT;
-                self.modulo
+                self.overflowed = true;
+                0
             } else { self.counter + 1 };
         }
         self.prev_counter_bit = counter_bit;
@@ -59,8 +68,8 @@ impl MemoryHandler for Timer {
     fn write(&mut self, addr: u16, value: u8) {
         match addr {
             0xFF04 => self.divider_counter = 0,
-            0xFF05 => self.counter = value,
-            0xFF06 => self.modulo = value,
+            0xFF05 => { if !self.reloaded { self.counter = value; } self.overflowed = false; },
+            0xFF06 => { self.modulo = value; if self.reloaded { self.counter = value;}},
             0xFF07 => { self.enabled = value & 0x04 != 0; self.clock_select = value & 0x03; }
             _ => panic!("Unexpected Address for Timer!"),
         }
