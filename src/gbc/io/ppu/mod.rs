0a4f7a4daf5d15db153b589f6b762afa7622646e
Akash Munagala <akash.munagala@gmail.com>
2020-03-18 17:15:46 -0700

    Support horizontal scrolling

--- src/gbc/io/ppu/mod.rs
+++ src/gbc/io/ppu/mod.rs
@@ -16,10 +16,10 @@ pub struct PPU {
     obj_enable: bool,
     bg_priority: bool,
     // Status
-    coincidence_int: bool,
-    oam_int: bool,
-    vblank_int: bool,
-    hblank_int: bool,
+    enable_coincidence_int: bool,
+    enable_oam_int: bool,
+    enable_vblank_int: bool,
+    enable_hblank_int: bool,
     coincidence_flag: bool,
     mode: u8,
     // Position and Scrolling
@@ -41,6 +41,11 @@ pub struct PPU {
     lcd_was_off: bool,
     y_coord_inc: u8,
 
+    coincidence_int: bool,
+    oam_int: bool,
+    vblank_int: bool,
+    hblank_int: bool,
+
     vram: [u8; 0x2000],
     screen: Screen,
 }
@@ -54,8 +59,8 @@ impl MemoryHandler for PPU {
             0xFF40 => shift!(lcd_enable, 7) | shift!(window_map_select, 6) | shift!(window_enable, 5) |
                       shift!(bg_window_tiles_select, 4) | shift!(bg_map_select, 3) | shift!(obj_size, 2) |
                       shift!(obj_enable, 1) | shift!(bg_priority, 0),
-            0xFF41 => shift!(coincidence_int, 6) | shift!(oam_int, 5) | shift!(vblank_int, 4) | shift!(hblank_int, 3) |
-                      shift!(coincidence_flag, 2) | self.mode,
+            0xFF41 => shift!(enable_coincidence_int, 6) | shift!(enable_oam_int, 5) | shift!(enable_vblank_int, 4) |
+                      shift!(enable_hblank_int, 3) | shift!(coincidence_flag, 2) | self.mode,
             0xFF42 => self.scroll_y,
             0xFF43 => self.scroll_x,
             0xFF44 => self.y_coord,
@@ -89,15 +94,26 @@ impl MemoryHandler for PPU {
                 }
             },
             0xFF41 => {
-                self.coincidence_int = value & (1 << 6) != 0;
-                self.oam_int = value & (1 << 5) != 0;
-                self.vblank_int = value & (1 << 4) != 0;
-                self.hblank_int = value & (1 << 3) != 0;
+                self.enable_coincidence_int = value & (1 << 6) != 0;
+                self.enable_oam_int = value & (1 << 5) != 0;
+                self.enable_vblank_int = value & (1 << 4) != 0;
+                self.enable_hblank_int = value & (1 << 3) != 0;
             },
             0xFF42 => self.scroll_y = value,
             0xFF43 => self.scroll_x = value,
             0xFF44 => {},
-            0xFF45 => self.y_coord_comp = value,
+            0xFF45 => {
+                self.y_coord_comp = value;
+                if self.lcd_enable {
+                    if self.y_coord == self.y_coord_comp {
+                        self.coincidence_flag = true;
+                        self.coincidence_int = true;
+                    } else {
+                        self.coincidence_flag = false;
+                        self.coincidence_int = false;
+                    }
+                }
+            },
             0xFF46 => {},
             0xFF47 => for i in 0..4 { self.bg_palette[i] = (value as usize >> 2 * i) & 0x3; },
             0xFF48 => self.obj_palette1 = value,
@@ -123,10 +139,10 @@ impl PPU {
             obj_enable: false,
             bg_priority: false,
             // Status
-            coincidence_int: false,
-            oam_int: false,
-            vblank_int: false,
-            hblank_int: false,
+            enable_coincidence_int: false,
+            enable_oam_int: false,
+            enable_vblank_int: false,
+            enable_hblank_int: false,
             coincidence_flag: false,
             mode: 0,
             // Position and Scrolling
@@ -147,6 +163,11 @@ impl PPU {
             lcd_was_off: false,
             y_coord_inc: 1,
 
+            coincidence_int: false,
+            oam_int: false,
+            vblank_int: false,
+            hblank_int: false,
+
             vram: [0; 0x2000],
             screen: Screen::new(sdl_ctx),
         }
@@ -165,10 +186,11 @@ impl PPU {
             self.mode = 0;
         }
 
-        let stat_signal = self.y_coord == self.y_coord_comp && self.coincidence_int ||
-                                self.mode == 0 && self.hblank_int ||
-                                self.mode == 2 && self.oam_int ||
-                                self.mode == 1 && (self.vblank_int | self.oam_int);
+        let stat_signal = self.coincidence_int || self.oam_int; || self.vblank_int || self.hblank_int;
+        self.coincidence_int = false;
+        self.oam_int = false;
+        self.vblank_int = false;
+        self.hblank_int = false;
         if !self.prev_stat_signal && stat_signal {
             interrupt |= IO::STAT_INT;
         }
@@ -187,39 +209,25 @@ impl PPU {
                     self.mode = 0;
                 }
             } else {
-                if self.clock_num == 79 { // OAM Search
-                    self.mode = 2;
-                    self.hblank_clock = 247 + self.scroll_x as u16 % 8;
-    
-                    let map_offset: u16 = if self.bg_map_select { 0x9C00 } else { 0x9800 };
-                    let y = self.y_coord.wrapping_add(self.scroll_y);
-                    for map_num in 0..20 {
-                        let map_addr = map_offset + (y as u16 / 8 * 32) + map_num;
-                        let tile_num = self.vram[map_addr as usize - 0x8000];
-                        let tile_addr = if self.bg_window_tiles_select {
-                            0x8000 + ((tile_num as usize) << 4)
-                        } else { 0x9000u16.wrapping_add((tile_num as i8 as u16) << 4) as usize };
-                        let tile_addr = tile_addr + 2 * (y as usize % 8);
-    
-                        let highs = self.vram[tile_addr - 0x8000];
-                        let lows = self.vram[tile_addr + 1 - 0x8000];
-                        for bit in 0..8 {
-                            let pixel_index = 3 * ((Screen::HEIGHT - 1 - self.y_coord as u32) * Screen::WIDTH + 
-                                                    map_num as u32 * 8 + bit) as usize;
-                            let high = (highs >> (7 - bit)) & 0x1;
-                            let low = (lows >> (7 - bit)) & 0x1;
-                            let color = PPU::SHADES[self.bg_palette[(high << 1 | low) as usize]];
-                            for i in 0..3 { self.screen.pixels[pixel_index + i] = color[i]; }
-                        }
+                if self.clock_num < 80 { // OAM Search
+                    // TODO: Do OAM Search
+                    if self.clock_num == 0 && self.y_coord != 0 {
+                        self.coincidence_flag = false;
                     }
-                } else if self.clock_num == self.hblank_clock {
-                    self.mode = 3;
-                } else if self.clock_num > self.hblank_clock {
-                    self.mode = 0;
+                    if self.clock_num == 4 {
+                        self.mode = 2;
+                    }
+                } else if self.clock_num >= 83 { // Pixel Transfer + HBlank
+                    if self.clock_num == 84 {
+                        self.mode = 3;
+                        self.render_line();
+                    } else if self.clock_num == self.hblank_clock {
+                        self.mode = 0;
+                    }
                 }
             }
         } else { // VBlank
-            if self.y_coord == 144 && self.clock_num == 0 {
+            if self.y_coord == 144 && self.clock_num == 4 {
                 self.screen.render();
                 interrupt = IO::VBLANK_INT;
             }
@@ -227,7 +235,14 @@ impl PPU {
                 self.y_coord = 0;
                 self.y_coord_inc = 0;
             }
-            self.mode = 1;
+            if self.y_coord != 144 || self.clock_num >= 4 {
+                self.mode = 1;
+                if self.enable_vblank_int || self.enable_oam_int { self.vblank_int = true; }
+            }
+        }
+
+        if self.y_coord != 0 && self.y_coord == self.y_coord_comp && self.enable_coincidence_int {
+            self.coincidence_int = true;
         }
 
         self.clock_num += 1;
@@ -238,5 +253,29 @@ impl PPU {
             self.lcd_was_off = false;
         }
         interrupt
+    }
+
+    fn render_line(&mut self) {
+        self.hblank_clock = 247 + self.scroll_x as u16 % 8;
+        let bg_map_offset: u16 = if self.bg_map_select { 0x9C00 } else { 0x9800 };
+        let y = self.y_coord.wrapping_add(self.scroll_y) as u16;
+        for x in 0..160 {
+            let map_x = (x + self.scroll_x as u16) / 8 % 32;
+            let bg_map_addr = bg_map_offset + (y / 8 * 32) + map_x;
+            let tile_num = self.vram[bg_map_addr as usize - 0x8000];
+            let tile_addr = if self.bg_window_tiles_select {
+                0x8000 + ((tile_num as usize) << 4)
+            } else { 0x9000u16.wrapping_add((tile_num as i8 as u16) << 4) as usize };
+            let tile_addr = tile_addr + 2 * (y as usize % 8);
+            let tile_highs = self.vram[tile_addr - 0x8000];
+            let tile_lows = self.vram[tile_addr + 1 - 0x8000];
+            let tile_x = (x + self.scroll_x as u16) % 8;
+            let high = (tile_highs >> (7 - tile_x)) & 0x1;
+            let low = (tile_lows >> (7 - tile_x)) & 0x1;
+
+            let pixel_index = 3 * ((Screen::HEIGHT - 1 - self.y_coord as u32) * Screen::WIDTH + x as u32) as usize;
+            let color = PPU::SHADES[self.bg_palette[(high << 1 | low) as usize]];
+            for i in 0..3 { self.screen.pixels[pixel_index + i] = color[i]; }
+        }
     }
 }
