681a141932f16e853497e753d5bf8f6f5fe8e405
Akash Munagala <akash.munagala@gmail.com>
2020-04-08 10:40:09 -0700

    Add RAM for ROM only

--- src/gbc/io/mbc/mod.rs
+++ src/gbc/io/mbc/mod.rs
@@ -11,12 +11,14 @@ pub trait MemoryBankController: MemoryHandler {
 
 pub fn get_mbc(header: Header, rom: Vec<u8>) -> Box<dyn MemoryBankController> {
     match header.get_cartridge_type() {
-        0x00 => Box::new(none::None::new(header, rom)),
+        0x00 => Box::new(none::None::new(header, rom, false, false)),
         0x01 => Box::new(mbc1::MBC1::new(header, rom, false, false)),
         0x02 => Box::new(mbc1::MBC1::new(header, rom, true, false)),
         0x03 => Box::new(mbc1::MBC1::new(header, rom, true, true)),
         0x05 => Box::new(mbc2::MBC2::new(header, rom, false)),
         0x06 => Box::new(mbc2::MBC2::new(header, rom, true)),
+        0x08 => Box::new(none::None::new(header, rom, true, false)),
+        0x09 => Box::new(none::None::new(header, rom, true, true)),
         _ => panic!("Unsupported Cartridge Type {}", header.get_cartridge_type()),
     }
 }
--- src/gbc/io/mbc/none.rs
+++ src/gbc/io/mbc/none.rs
@@ -4,12 +4,20 @@ use super::Header;
 
 pub struct None {
     rom: Vec<u8>,
+    ram: [u8; 0x2000],
+
+    has_ram: bool,
+    _has_battery: bool,
 }
 
 impl None {
-    pub fn new(_header: Header, rom: Vec<u8>) -> Self {
+    pub fn new(_header: Header, rom: Vec<u8>, has_ram: bool, has_battery: bool) -> Self {
         None {
             rom,
+            ram: [0; 0x2000],
+
+            has_ram,
+            _has_battery: has_battery,
         }
     }
 }
@@ -18,14 +26,14 @@ impl MemoryHandler for None {
     fn read(&self, addr: u16) -> u8 {
         if addr < 0x8000 {
             self.rom[addr as usize]
-        } else {
-            0xFF // TODO: Add External RAM Support
-        }
+        } else if self.has_ram {
+            self.ram[addr as usize - 0xA000]
+        } else { 0xFF }
     }
     
-    fn write(&mut self, addr: u16, _value: u8) {
-        if addr > 0x8000 {
-            panic!("External RAM not supported!");
+    fn write(&mut self, addr: u16, value: u8) {
+        if addr >= 0xA000 && addr < 0xC000 && self.has_ram {
+            self.ram[addr as usize - 0xA000] = value;
         }
     }
 }
